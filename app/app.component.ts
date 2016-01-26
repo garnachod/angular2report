28368498ad6150f4b10bb117c525cd266cde6a92
Diego Castaño Chillarón <diego.castano@alumnos.upm.es>
2016-01-26 11:21:47 +0100

    init

---
+++ app/app.component.ts
@@ -0,0 +1,21 @@
+import {Component} from  'angular2/core';
+import {OnInit} from 'angular2/core';
+
+@Component({
+    selector: 'app',
+    directives: [],
+    providers: [],
+    template: `
+
+        <report></report>
+    
+    `
+})
+export class AppComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
